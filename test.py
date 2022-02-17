f2a16ef071f63651659436ed8ac4c1a3aa94cfce
Maksim <56311335+remix2021687@users.noreply.github.com>
2022-02-17 22:29:34 +0300

    ProjectWebCreation

---
+++ test.py
@@ -0,0 +1,70 @@
+import os, random
+
+print("Программа создана для web проектов")
+print("Создать проект ---- Удалить проект ---- Переиминовать Проект")
+Answer_1 = input("Выбор: ")
+
+if Answer_1 == "Создать проект":
+    Answer_2 = input("Как вы хочите ее назвать: ")
+    Answer_3 = input("Вам нужен Jquery: ")
+    print("Ваш проект создан по этом пути: ", os.getcwd())
+
+    os.mkdir(Answer_2)
+    os.makedirs(f"{Answer_2}/css")
+    os.makedirs(f"{Answer_2}/img")
+    os.makedirs(f"{Answer_2}/js")
+
+    index_html = open(f"{Answer_2}/index.html", "w")
+    index_html.write("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <title>Document</title>
+</head>
+<body>
+    <script src="js/index.js"></script>
+</body>
+</html>
+    """)
+
+    if Answer_3 == "Да":
+        index_html = open(f"{Answer_2}/index.html", "w")
+        index_html.write("""
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <script src="js/index.js"></script>
+</body>
+</html>
+        """)
+    index_css = open(f"{Answer_2}/css/index.css", "w")
+    index_js = open(f"{Answer_2}/js/index.js", "w")
+
+elif Answer_1 == "Переиминовать Проект":
+    Answer_2 = input("По какому пути находится: ")
+    Answer_3 = input("Новое имя для проекта: ")
+    os.rename(f"{Answer_2}", f"{Answer_3}")
+
+elif Answer_1 == "Удалить проект":
+    Answer_2 = input("По какому пути нахожится ваш проект: ")
+    os.remove(f"{Answer_2}/index.html")
+    os.remove(f"{Answer_2}/js/index.js")
+    os.remove(f"{Answer_2}/css/index.css")
+    os.removedirs(f"{Answer_2}/js")
+    os.removedirs(f"{Answer_2}/img")
+    os.removedirs(f"{Answer_2}/css")
+    print("Ваш проект был успешно удален")
+
+input()
