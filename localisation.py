f16938d87278779a3e8e2e3402fee10ae6d816ad
remix2021687 <erominmaksim@gmail.com>
2023-02-10 19:23:59 +0100

    update

---
+++ localisation.py
@@ -0,0 +1,14 @@
+take_lang = {
+    "sentence_1": "Виберіть мову || Choose language",
+    "sentence_2": "Українська || English: ",
+}
+
+word_ua = {
+    "sentence_1": "Програма створена для веб-розробників",
+    "sentence_2": "Створити проект ---- Видалити проект ---- Перейменувати Проект",
+    "sentence_3": "Вибір: ",
+    # "sentence_4": "",
+    # "sentence_5": ""
+}
+
+word_en = {}
